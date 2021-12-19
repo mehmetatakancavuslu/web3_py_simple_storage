0bb4d26189b0943fbb386b65aa3a7fae0cc2e73b
mehmetatakancavuslu <atakancavuslu322@gmail.com>
2021-12-19 19:09:06 +0300

    Add interacting functionality with the contract

--- deploy.py
+++ deploy.py
@@ -64,6 +64,34 @@ transaction = SimpleStorage.constructor().buildTransaction(
 )
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 # Send this signed transaction
+print("Deploying contract...")
 txh_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
 # Wait for block confirmation
 tx_receipt = w3.eth.wait_for_transaction_receipt(txh_hash)
+print("Deployed!")
+
+# Working with the contract, we always need 1)Contract Adress 2)Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+
+# Call -> Simulate making the call and getting a return value (no state change)
+# Transact -> make a state change
+
+# Initial value of favorite number
+print(simple_storage.functions.retrieve().call())
+
+print("Updating Contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {
+        "chainId": chain_id,
+        "from": my_address,
+        "nonce": nonce + 1,
+        "gasPrice": w3.eth.gas_price,
+    }
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+print(simple_storage.functions.retrieve().call())
